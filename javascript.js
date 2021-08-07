dbce170fbabd9ae88132e13b56779db3102932cf
wwwAJun <73609544+wwwAJua@users.noreply.github.com>
2021-08-08 02:40:31 +0300

    ПеременныеТипыОперации
    Переменные
    Типы
    Операции

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var num1 = 12
+var num2 = 8
+
+console.log('+:', num1 + num2)
+console.log('-:', num1 - num2)
+console.log('*:', num1 * num2)
+console.log('/:', num1 / num2)
+
+var str1 = 'Hello'
+var str2 = 'World'
+
+console.log('+:', str1 + ' ' + str2)
+
+console.log('1 + 2:', 1 + '2')
+console.log("str1 + num1:", str1 + num2)
+
+console.log('Boolean + 1:', true + num1)
+
+console.log('true + 1:', true + 1)
+console.log('false + 1:', false + 1)
