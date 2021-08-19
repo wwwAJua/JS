3a7f10a1852dd7921d5281812adf86f24a1a8b7c
wwwAJun <73609544+wwwAJua@users.noreply.github.com>
2021-08-19 20:56:47 +0300

    Events

--- javascript.js
+++ javascript.js
@@ -1,46 +1,14 @@
-// let div = document.getElementById('back')
-// let p = document.getElementsByClassName('text')
-// let h1 = document.getElementsByTagName('h1')
-// console.log(div);
-// console.log(p);
-// console.log(h1);
-
-// class -> .CLASS_NAME
-// id -> #ID
-
-// let div = document.querySelector('#back')
-// let p = document.querySelectorAll('.text')
-// let h1 = document.querySelector('h1')
-// let ul1 = document.querySelector('#back ul')
-// let ul2 = document.querySelector('#back > div ul')
-// console.log(div);
-// console.log(p);
-// console.log(h1);
-// console.log(ul1);
-// console.log(ul2);
-
-let div = document.querySelector('#back')
-let h1 = document.querySelector('h1')
+let button = document.querySelector('button')
 let input = document.querySelector('input')
-div.innerHTML = '<h2 style = "color: red;">From JavaScript with love</h2>'//меняем целый DIV
-h1.textContent = 'Changet from JS'//меняем контент в тэге
-input.value = 'This is JS'
+let h1 = document.querySelector('h1')
 
-let a = document.querySelector('a')
-a.setAttribute('href', 'https://google.com/maps')
-a.setAttribute('title', 'go to maps')
-a.textContent = 'Google Maps'
+button.addEventListener('click', function () {
+	h1.textContent = input.value
+})
 
-let box1 = document.querySelector('#box1')
-let box2 = document.querySelector('#box2')
-
-box1.classList.add('red')
-box2.classList.remove('green')
-
-let checkclass = box2.classList.contains('green')
-
-if (checkclass) {
-
-} else {
-	box2.classList.add('green')
-}
+h1.addEventListener('mouseenter', function () {
+	this.style.color = 'orange'
+})
+h1.addEventListener('mouseleave', function () {
+	this.style.color = 'white'
+})
